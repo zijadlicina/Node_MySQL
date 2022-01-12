2572985de53f20a82f60709db9807f4d5839cc54
userName1 <hello@gmail.com>
2022-01-12 01:53:09 +0100

    Creating table DB

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     host        : 'localhost',
     user        : 'root',
     password    : '',
-  //  database    : 'nodemysql'
+    database    : 'nodemysql'
 });
 
 // Connect
@@ -26,6 +26,16 @@ app.get('/createdb', (req, res)=>{
     })
 })
 
-app.listen(300 , ()=>{
+// Create table
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table created...')
+    })
+})
+
+app.listen(300, ()=>{
     console.log(`Server started on port 3000`);
 })

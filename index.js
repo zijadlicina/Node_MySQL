b322b431150b2b9df19660832a7845fd8bedd224
userName1 <hello@gmail.com>
2022-01-12 01:40:18 +0100

    Creating DB from app

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ db.connect((err) => {
     console.log("MySQL Connected...")
 })
 
+// Create DB
+app.get('/createdb', (req, res)=>{
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    })
+})
+
 app.listen(300 , ()=>{
     console.log(`Server started on port 3000`);
 })
